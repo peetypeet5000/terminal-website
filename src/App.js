f086fcef51d153aa562ee391b7fb83803964e400
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-04-05 00:32:04 -0700

    started styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import reactDom from 'react-dom';
+import { render } from '@testing-library/react';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Terminal extends React.Component {
+
+  render() {
+    return (
+      <div class="terminal">
+        <pre>Terminal Test.</pre>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <body>
+          <Terminal />
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
