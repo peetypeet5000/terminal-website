133dce3525411191462363ee3d2cd758f8220a6a
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-05-02 13:28:04 -0700

    more fades

--- src/About.js
+++ src/About.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import FadeIn from 'react-fade-in';
 import './About.css'
 
 
 export default class About extends React.Component {
     render() {
       return (
-        <div className="about-container">
-          <h2>About Me</h2>
-          <p className="about-element">Hi! My name is Peter LaMontagne and I am currently a Sophmore Computer Science Student
-           at Oregon State Univeristy. I am in the computer systems option, with the goal of becoming a Software Engineer
-            after gradution.</p>
-          <p className="about-element">In my free time I like to cook, play piano, run, and read.</p>
-          <img className="about-photo" src="./chicago.jpg" alt="Me in Chicago"/>
-        </div>
+          <div className="about-container">
+            <FadeIn>
+              <h2>About Me</h2>
+              <p className="about-element">Hi! My name is Peter LaMontagne and I am currently a Sophmore Computer Science Student
+              at Oregon State Univeristy. I am in the computer systems option, with the goal of becoming a Software Engineer
+                after gradution.</p>
+              <p className="about-element">In my free time I like to cook, play piano, run, and read.</p>
+              <div className="about-photo">
+                <img className="about-photo" src="./chicago.jpg" alt="Me in Chicago"/>
+              </div>
+            </FadeIn>
+          </div>
       );
     }
   }
--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FadeIn from 'react-fade-in';
 import './About.css'
 
 
@@ -6,11 +7,13 @@ export default class Contact extends React.Component {
     render() {
       return (
         <div className="about-container">
-          <h2>Contact Me</h2>
-          <div className="about-element big"><span className="about-emphesis">Email: </span>peter@peterlamontagne.com</div>
-          <div className="about-element big"><span className="about-emphesis">Phone: </span>(503) - 853 - 4419</div>
-          <div className="about-element big"><span className="about-emphesis">LinkedIn: </span><a href="http://www.linkedin.com/in/peter-lamontagne">LinkedIn.com/in/Peter-LaMontagne</a></div>
-          <div className="about-element big"><span className="about-emphesis">GitHub: </span><a href="https://github.com/peetypeet5000">GitHub.com/Peetypeet5000</a></div>
+          <FadeIn>
+            <h2>Contact Me</h2>
+            <div className="about-element big"><span className="about-emphesis">Email: </span>peter@peterlamontagne.com</div>
+            <div className="about-element big"><span className="about-emphesis">Phone: </span>(503) - 853 - 4419</div>
+            <div className="about-element big"><span className="about-emphesis">LinkedIn: </span><a href="http://www.linkedin.com/in/peter-lamontagne">LinkedIn.com/in/Peter-LaMontagne</a></div>
+            <div className="about-element big"><span className="about-emphesis">GitHub: </span><a href="https://github.com/peetypeet5000">GitHub.com/Peetypeet5000</a></div>
+            </FadeIn>
           <br/>
         </div>
       );
