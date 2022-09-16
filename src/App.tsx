e19f030b8651556151ac9b24ccb608206ecbc07e
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2022-09-16 12:33:35 -0700

    encapsulate in terminal window

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,34 @@ export default function App () {
   const location = useLocation();
 
   return (
-    <div>
+    <>
       <video autoPlay muted loop playsInline id="background-vid">
         <source src="./compressed_bkg.mp4" type="video/mp4" />
       </video>
       <Name text={'>Peter LaMontagne'} />
-      <div className="terminal__outer">
-        <div className="terminal">
-          <pre className="terminal__title">{location.pathname}</pre>
-          <Outlet />
-          {location.pathname != '/' && 
-            <Link className='menu__button menu__button__return' to='/'>Return to Menu </Link>
-          }
+      <div className='window'>
+        <div className='window__bar'>
+          <div className='window__bar-circles'>
+            <div className='window__bar-circle'></div>
+            <div className='window__bar-circle window__bar-circle__yellow' />
+            <div className='window__bar-circle window__bar-circle__green' />
+          </div>
+          <div>
+            /setup - root@localhost - user-config
+          </div>
+          <div className='window__bar-filler' />
+        </div>
+        <p className='window__text'>Raspberry Pi 3 Model B Rev 2.1</p>
+        <div className="terminal__outer">
+          <div className="terminal">
+            <pre className="terminal__title">{location.pathname}</pre>
+            <Outlet />
+            {location.pathname != '/' && 
+              <Link className='menu__button menu__button__return' to='/'>Return to Menu </Link>
+            }
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,7 +5,7 @@ export default function Menu () {
   return (
     <div className='menu'>
       <Link className='menu__button' to='/resume'>Resume  ---  View my resume</Link>
-      <Link className='menu__button' to='/projects'>Projects ---  See the various personal and class projects I have worked on</Link>
+      <Link className='menu__button' to='/projects'>Projects ---  See various personal and class projects I have worked on</Link>
       <Link className='menu__button' to='/about'>About  ---  Read more about me</Link>
       <Link className='menu__button' to='/contact'>Contact  ---  Connect with me</Link>
     </div>
