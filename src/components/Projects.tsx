3978dda834b856f45bddc57c15263a1369c1a2cd
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2023-04-11 15:15:30 -0700

    add more projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,14 +8,14 @@ export default function Projects(): ReactElement {
       {projects.reverse().map((project, index) => {
         return (
         <div className="project__card" key={index}>
-          <h2>Project {project.number}: {project.projectTitle}</h2>
+          <h2>Project {index + 1}: {project.projectTitle}</h2>
 
           <div className="project__content">
             <img className="project__image" src={project.projectImage} alt={project.projectImageAlt}/>
             <p className="project__text">
               {project.projectDescription}
                 <br/><br/>
-                See the GitHub repo for this project <a href={project.projectGithub}>here.</a>
+                See the project details <a href={project.projectGithub}>here.</a>
               </p>
           </div>
         </div>);
